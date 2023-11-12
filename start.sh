10e5c9c674bd3403b93ac5472c0e34c63177fb19
gafar-code <gafartok@gmail.com>
2023-11-12 09:03:53 +0700

    Use RDS to deploy Docker hub

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "run db migration"
-/app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
+/app/migrate -path /app/migration -database "postgresql://root:secret@3.94.76.231:5432/store_db?sslmode=disable" -verbose up
 
 echo "start the app"
 exec "$@"
